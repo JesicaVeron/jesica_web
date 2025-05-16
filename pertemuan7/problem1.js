83e1f425cead34abb5a40ddae2677eaaaa566e45
JesicaVeron <2403310718@students.satyaterrabhinneka.ac.id>
2025-05-16 19:16:54 +0700

    pertemuan7

---
+++ pertemuan7/problem1.js
@@ -0,0 +1,9 @@
+// Problem 1 - String Concatenation
+let firstName = 'Jesica Veron';
+let lastName = 'Br Purba';
+let bornYear = 2005;
+
+let currentYear = 2025;
+let age = currentYear - bornYear;
+
+console.log('Hello, ' + firstName + ' ' + lastName + '! You are ' + age + ' years old.')
